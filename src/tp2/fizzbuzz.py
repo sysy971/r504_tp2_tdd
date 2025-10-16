28e1d516efb4e1d074ad570b3025a6c4c9b656d0
sysy971 <sylvain.seremes@gmail.com>
2025-10-16 16:39:23 +0200

    feat(fizzbuzz): affiche(n) 1..n (PASS)

--- src/tp2/fizzbuzz.py
+++ src/tp2/fizzbuzz.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def _token(n: int) -> str:
     if n % 15 == 0:
         return "FrisBee"
@@ -7,5 +9,11 @@ def _token(n: int) -> str:
         return "Buzz"
     return str(n)
 
-def affiche():
-    return "".join(_token(i) for i in range(1, 101))
+def affiche(n: Optional[int] = None, n2: Optional[int] = None):
+    if n is None and n2 is None:
+        start, end = 1, 100
+    elif n is not None and n2 is None:
+        start, end = 1, int(n)
+    else:
+        start, end = 1, 100
+    return "".join(_token(i) for i in range(start, end + 1))
