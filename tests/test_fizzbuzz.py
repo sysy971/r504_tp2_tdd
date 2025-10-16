4e130e37c0b589a9ed04267e78f95d9ec9de9caf
sysy971 <sylvain.seremes@gmail.com>
2025-10-16 16:49:51 +0200

    test(fizzbuzz): affiche(n1,n2) (FAIL)

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -10,3 +10,7 @@ def test_affiche_sans_param_retourne_chaine_100():
 def test_affiche_avec_n():
     from src.tp2.fizzbuzz import affiche
     assert affiche(15) == "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
+def test_affiche_intervalle():
+    from src.tp2.fizzbuzz import affiche
+    assert affiche(5, 10) == "BuzzFizz78FizzBuzz"
+    assert affiche(10, 16) == "Buzz11Fizz1314FrisBee16"
